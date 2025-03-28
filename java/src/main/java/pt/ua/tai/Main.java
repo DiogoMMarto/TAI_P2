bd0b2d78c6f0bdbc3bb8b58b8c9eebcf4d6e0d8f
ilkeratik <ilkeratik35@gmail.com>
2025-03-28 09:42:34 +0000

    minus total sum for bps

--- java/src/main/java/pt/ua/tai/Main.java
+++ java/src/main/java/pt/ua/tai/Main.java
@@ -15,7 +15,7 @@ public class Main {
             String metaContent = readTxtFileToString("sequences/meta.txt");
             Meta meta = new Meta(metaContent, 2);
             Map<String, Double> best = meta.getBestSequences(dbMap, 0.1f, 20);
-            best.forEach((x, y) -> System.out.println(x + "\t" + y));
+            best.forEach((x, y) -> System.out.println(y + "\t" + x));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
