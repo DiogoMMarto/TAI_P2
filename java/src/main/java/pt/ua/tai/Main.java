9f41dbdd212018c9df1abb2bb3f27a5ed916f1b7
ilkeratik <ilkeratik35@gmail.com>
2025-03-28 09:09:32 +0000

    update

--- java/src/main/java/pt/ua/tai/Main.java
+++ java/src/main/java/pt/ua/tai/Main.java
@@ -2,6 +2,7 @@ package pt.ua.tai;
 
 import pt.ua.tai.meta.Meta;
 
+import java.util.List;
 import java.util.Map;
 
 import static pt.ua.tai.database.DatabaseReader.readFileAndCreateDbMap;
@@ -12,9 +13,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             Map<String, String> dbMap = readFileAndCreateDbMap("sequences/db.txt");
-            String metaContent = readTxtFileToString(dbMap.get("sequences/meta.txt"));
+            String metaContent = readTxtFileToString("sequences/meta.txt");
             Meta meta = new Meta(metaContent, 2);
-            meta.batchRun(dbMap, 0.1f);
+            List<String> best = meta.getBestSequences(dbMap, 0.1f, 20);
+            best.forEach(x -> System.out.println(x));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
